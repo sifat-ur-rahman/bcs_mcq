04cd099856706fef16303a2754a45722e1461a31
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-13 17:07:24 +0600

    add

--- src/Components/QuestionCard/QuestionCard.jsx
+++ src/Components/QuestionCard/QuestionCard.jsx
@@ -1,15 +1,15 @@
 
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { FaEdit, } from "react-icons/fa";
-import { IconName, MdDelete } from "react-icons/md";
+import {  MdDelete } from "react-icons/md";
 
 function QuestionCard() {
   return (
     <div>
         
-        <div className='border-2 border-gray-400 p-2'>
+        <div className='border-2 border-gray-400 p-2 rounded-md'>
             <div className="flex items-center justify-between">
-                <input type="checkbox"  className="checkbox checkbox-md" />
+                <input type="checkbox"  className="checkbox checkbox-info" />
                 <p>1</p>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="m-1"><BiDotsVerticalRounded/></label>
